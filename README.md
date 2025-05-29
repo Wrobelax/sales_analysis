27fdc22c4167222ccac673bc8c08d983d5135d19
Adrian <awrobel.wr@gmail.com>
2025-05-29 20:38:49 +0200

    Modified script 'modeling.py'. Generated 'average_sale.png', product_per_order.png' and 'sale_per_client.png' of it. Modivied 'README.md' - added newly created files.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+**Project status**: _In progress_ - data visualisation phase.
+
 This project is a data analysis of a publicly available data from: 
 https://www.kaggle.com/datasets/carrie1/ecommerce-data
 
@@ -5,19 +7,22 @@ https://www.kaggle.com/datasets/carrie1/ecommerce-data
 The project consists of cleaning data, data analysis and data modeling. All data was pushed and managed on Github via Git bash.
 
 
-Structure of the project is as follows:
+**Structure of the project is as follows**:
 
-Folder "data" consists of files:
+_Folder "data" consists of files_:
 - "data.csv" : Input data
 - "cleaned_data.csv" : Data after cleaning. Script used for is in scripts/exploration_cleaning.py
 
 
-Folder "scripts" covers scripts written and used for the project. Some of the code was left commented for reference:
+_Folder "scripts" covers scripts written and used for the project. Some of the code was left commented for reference:_
 - "exploration_cleaning.py" : Script used for creation of the "cleaned_data.csv". Input data used from "data.csv" file. Removed unnecessary data, modified some data types and explored data for potential use.
 - "data_analysis.py" : Script using "cleaned_data.csv" file. Covers data analysis, further exploration and some basic visualisation.
 
 
-Folder "outputs" covers charts resulted of analysis:
+_Folder "outputs" covers charts resulted of analysis:_
 - "orders_per_country.png" : Bar chart covering top 10 countries by order volume. Generated from "data_analysis.py" script.
 - "orders_per_hr.png" : Line chart showing average number of orders per hour. Generated from "data_analysis.py" script.
 - "orders_per_mth.png" : Line chart showing average number of orders per month. Generated from "data_analysis.py" script.
+- "average_sale.png" : Histograph showing average order values.
+- "product_per_order.png" : Histograph showing number of products per order.
+- "sale_per_client.png" : Histograph showing average basket value per client.
--- scripts/modeling.py
+++ scripts/modeling.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
 sns.set(style = "whitegrid")
 
@@ -13,17 +14,18 @@ df = pd.read_csv(url, encoding = "ISO-8859-1", dtype = {"InvoiceNo" : str})
 """Data modeling."""
 # Visualisation of average order price - removing outliers.
 df["TotalPrice"] = df["Quantity"] * df["UnitPrice"]
+df["InvoiceDate"] = pd.to_datetime(df["InvoiceDate"], errors = "coerce")
 order_values = df.groupby("InvoiceNo")["TotalPrice"].sum()
 order_values = order_values[order_values < 1000]
-order_values = order_values[order_values > 1]
 # print(order_values.describe())
 
-# plt.figure(figsize = (10,6))
-# sns.histplot(order_values, bins = 50, kde = True)
-# plt.title("Order values")
-# plt.xlabel("£")
-# plt.ylabel("Frequency")
-# plt.tight_layout()
+plt.figure(figsize = (10,6))
+sns.histplot(order_values, bins = 50, kde = True)
+plt.title("Order values")
+plt.xlabel("£")
+plt.ylabel("Frequency")
+plt.xlim(0,1000)
+plt.tight_layout()
 # plt.savefig("https://github.com/Wrobelax/sales_analysis.git/outputs/average_sale.png") # Saving results to file
 
 
@@ -34,7 +36,6 @@ top_clients = client_values.sort_values(ascending = False).head(10)
 customer_total = df.groupby("CustomerID")["TotalPrice"].sum()
 customer_orders = df.groupby("CustomerID")["InvoiceNo"].nunique()
 avg_basket_val = customer_total / customer_orders
-avg_basket_val = avg_basket_val[avg_basket_val > 1]
 avg_basket_val = avg_basket_val[avg_basket_val < 1000]
 
 plt.figure(figsize = (10,6))
@@ -42,6 +43,37 @@ sns.histplot(avg_basket_val, bins = 50, kde = True)
 plt.title("Average basket value per customer")
 plt.xlabel("£")
 plt.ylabel("number of customers")
+plt.xlim(0,1000)
 plt.tight_layout()
-# plt.show()
-# plt.savefig("C:\\Users\\adiw\\PycharmProjects\\Kurs\\sales_analysis\\outputs\\sale_per_client.png") # Saving results to file
+# plt.savefig("https://github.com/Wrobelax/sales_analysis.git/outputs/sale_per_client.png") # Saving results to file
+
+
+# Visualisation of products per order.
+product_per_order = df.groupby("InvoiceNo")["Quantity"].sum()
+product_per_order = product_per_order[product_per_order < 1000]
+
+plt.figure(figsize = (10,6))
+sns.histplot(product_per_order, bins = 50, kde = True)
+plt.title("Number of products per order")
+plt.xlabel("Items")
+plt.ylabel("Frequency")
+plt.xlim(0,1000)
+plt.tight_layout()
+# plt.savefig("https://github.com/Wrobelax/sales_analysis.git/outputs/product_per_order.png") # Saving results to file
+
+
+#Visualisation of sales basing on month and country - limited to top 10.
+df["Month"] = df["InvoiceDate"].dt.to_period("M").astype(str)
+p_table = df.pivot_table(index = "Country", columns = "Month", values = "TotalPrice", aggfunc = "sum").fillna(0)
+top_countries = p_table.sum(axis = 1).sort_values(ascending = False).head(10).index
+p_table = p_table.loc[top_countries]
+
+p_table_log = np.log1p(p_table) # Using log scale to add 1 and avoid log(0)
+
+plt.figure(figsize = (12,6))
+sns.heatmap(p_table, cmap = "YlGnBu", linewidths = 0.5, annot = True, fmt = ".1f")
+plt.title("Sales per Country/Month")
+plt.xlabel("Month")
+plt.ylabel("Country")
+plt.tight_layout()
+plt.show()
